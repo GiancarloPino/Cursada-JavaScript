169ee7b1a18cbe045a6282f4edfc70a9dbd3701b
Gian <gianc.ps.19@outlook.com>
2024-06-07 14:26:04 -0500

    agregado fetch, css

--- js/main.js
+++ js/main.js
@@ -1,67 +1,166 @@
-function calcularDescuento(precio) {
-  const descuentos = [
-    { rango: 200, porcentaje: 0.15 },
-    { rango: 100, porcentaje: 0.1 },
-    { rango: 50, porcentaje: 0.05 },
-  ];
+const container = document.getElementById("contenedor");
+const btnProducto = document.getElementById("boton-producto");
+const btnCarrito = document.getElementById("boton-carrito");
+const btnBorrar = document.getElementById("boton-borrar-carrito");
+const btnPagar = document.getElementById("boton-pagar");
 
-  for (let i = 0; i < descuentos.length; i++) {
-    if (precio >= descuentos[i].rango) {
-      return precio * descuentos[i].porcentaje;
+let arrayProducto = [];
+
+// pagar los productos en el carrito
+function pagar(elCarrito) {
+    let sumaPrecios = elCarrito.reduce((acumulador, el) => acumulador += el.price, 0);
+    
+    if (sumaPrecios) {
+        Swal.fire(`El total a pagar por sus productos es: ${sumaPrecios}`);
+    } else {
+        Swal.fire(`Usted aún no agregó nada a su carrito :(`);
     }
-  }
-  return 0;
-}
-//iniciando funcion
-function mostrarProductosConDescuento(event) {
-  event.preventDefault();  // que no se actualice
-
-  let formulario = event.target;
-  let nombreProducto = formulario.children[0].value; //recibiendo por imput
-  let precioProducto = parseInt(formulario.children[1].value); //recibiendo por input y transformando a numero
-
-  let productos = JSON.parse(localStorage.getItem('productos')) || []; //funcion or, nota: agregar mas func orden superior
-  const hrInicio = { minuto: new Date().getMinutes(), segundo: new Date().getSeconds() };
-  const xHoraInicio = hrInicio.minuto * 60 + hrInicio.segundo;
-
-  let descuento = calcularDescuento(precioProducto);
-  let precioFinal = precioProducto - descuento;
-
-  productos.push({
-    nombre: nombreProducto,
-    precio: precioProducto,
-    descuento: descuento,
-    precioFinal: precioFinal
-  });
-
-  localStorage.setItem('productos', JSON.stringify(productos));
-
-  const hrFin = { minuto: new Date().getMinutes(), segundo: new Date().getSeconds() };
-  const xHoraFin = hrFin.minuto * 60 + hrFin.segundo;
-  const sumaPrecios = productos.reduce((acumulador, el) => acumulador += el.precioFinal, 0);
-  const sumaDescuentos = productos.reduce((acumulador, el) => acumulador += el.descuento, 0);
-
-  let listaProductos = "";
-  productos.forEach(producto => {
-    listaProductos += `Producto: ${producto.nombre}\nPrecio: ${producto.precio}\nDescuento: ${producto.descuento}\nA pagar: ${producto.precioFinal}\n`;
-  });
-
-  let main = document.getElementById("main");
-  const parrafo = document.createElement("p");
-
-  parrafo.innerText = `\n${listaProductos}\nTotal a pagar: ${sumaPrecios}\nUsted se ha ahorrado: ${sumaDescuentos}\nUsted solo tardo: ${(xHoraFin - xHoraInicio)} segundos!\nVuelva Pronto!!!`;
-
-  main.appendChild(parrafo);
-  
 }
 
-console.log(localStorage);
-const formulario = document.getElementById("formulario");
-formulario.addEventListener("submit", mostrarProductosConDescuento);
-const boton2 = document.getElementById("btn-limpiar");
-boton2.onclick = function() {
-  localStorage.clear();
-};
+// una alerta cuando se agrega un producto al carrito
+function alertaToasty() {
+    Toastify({
+        text: "Agregado correctamente al carrito",        
+        duration: 3000
+    }).showToast();
+}
 
+// botón de borrar el carrito
+btnBorrar.addEventListener("click", function() {
+    Swal.fire({
+        title: "¿Estás seguro que quieres borrar tu carrito?",
+        text: "No podrás recuperar tu carrito actual, ¿estás seguro?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, borrar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.clear();
+            Swal.fire({
+                title: "Borrado!",
+                text: "Borraste tu carrito :(",
+                icon: "success"
+            });
+            location.reload();
+        }
+    });    
+});
 
+const fetchProductos = fetch("https://fakestoreapi.com/products"); // recibiendo los productos de la API
 
+const carrito = JSON.parse(localStorage.getItem("carritoStorage")) || [];
+
+// Función para agregar productos al carrito
+function agregarAlCarrito(producto) {
+    const itemCarrito = {
+        title: producto.title,
+        price: producto.price,
+        foto: producto.image,
+    };
+    carrito.push(itemCarrito);
+    localStorage.setItem('carritoStorage', JSON.stringify(carrito)); // Guardar el carrito actualizado
+    console.log(carrito); // Verificar el carrito en la consola
+}
+
+// Función para mostrar el carrito
+function mostrarCarrito(carrito) {
+    container.innerHTML = "";
+    carrito.forEach(itemCarrito => { // Iterar sobre el arreglo carrito
+        const tarjetas = document.createElement('div');
+        tarjetas.className = "tarjeta";
+        container.appendChild(tarjetas);
+        
+        const img = document.createElement('img');
+        img.className = "product-imagen";
+        img.src = itemCarrito.foto; // Acceder a la propiedad foto de cada itemCarrito
+        
+        const divTarjeta = document.createElement('div');
+        divTarjeta.className = "product-detalle";
+        
+        const h2Titulo = document.createElement('h2');
+        h2Titulo.className = "product-titulo";
+        h2Titulo.innerText = itemCarrito.title; // Acceder a la propiedad title de cada itemCarrito
+        
+        const pDescripcion = document.createElement('p');
+        pDescripcion.className = "product-descripcion";
+        pDescripcion.innerText = "Actualmente en tu carrito"; // Acceder a la propiedad description de cada itemCarrito
+        
+        const pPrecio = document.createElement('p');
+        pPrecio.className = "product-precio";
+        pPrecio.innerText = "$ " + itemCarrito.price; // Acceder a la propiedad price de cada itemCarrito
+        
+        container.appendChild(tarjetas);
+        tarjetas.appendChild(divTarjeta);
+        tarjetas.appendChild(img);
+        
+        divTarjeta.appendChild(h2Titulo);
+        divTarjeta.appendChild(pDescripcion);
+        divTarjeta.appendChild(pPrecio);
+    });
+}
+
+// Fetch de productos y almacenamiento en arrayProducto
+fetchProductos
+    .then(response => response.json())
+    .then((data) => {
+        arrayProducto = data; // almacenar los datos en arrayProducto
+    })
+    .catch((error) => {
+        console.error('Error al obtener los productos:', error);
+    });
+
+btnProducto.addEventListener("click", () => {
+    container.innerHTML = "";
+    arrayProducto.forEach(el => {
+        const tarjetas = document.createElement('div');
+        tarjetas.className = "tarjeta";
+        container.appendChild(tarjetas);
+        
+        const img = document.createElement('img');
+        img.className = "product-imagen";
+        img.src = el.image;
+        
+        const divTarjeta = document.createElement('div');
+        divTarjeta.className = "product-detalle";
+        
+        const h2Titulo = document.createElement('h2');
+        h2Titulo.className = "product-titulo";
+        h2Titulo.innerText = el.title;
+        
+        const pDescripcion = document.createElement('p');
+        pDescripcion.className = "product-descripcion";
+        pDescripcion.innerText = el.description;
+        
+        const pPrecio = document.createElement('p');
+        pPrecio.className = "product-precio";
+        pPrecio.innerText = "$ " + el.price;
+        
+        const boton = document.createElement('button');
+        boton.className = "boton";
+        boton.innerText = "Agregar al carrito";
+
+        boton.addEventListener('click', () => {
+            alertaToasty();
+            agregarAlCarrito(el);
+        });
+        
+        container.appendChild(tarjetas);
+        tarjetas.appendChild(divTarjeta);
+        tarjetas.appendChild(img);
+        tarjetas.appendChild(boton);
+        
+        divTarjeta.appendChild(h2Titulo);
+        divTarjeta.appendChild(pDescripcion);
+        divTarjeta.appendChild(pPrecio);
+    });
+});
+
+btnCarrito.addEventListener("click", () => {
+    mostrarCarrito(carrito); // pasa el carrito como argumento
+});
+
+btnPagar.addEventListener("click", () => {
+    pagar(carrito); // pasa el carrito como argumento
+});
